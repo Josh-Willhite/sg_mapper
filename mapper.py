ba44778d0d26c222a7f6e8eeca491e48973f28d2
josh <jwillhite@gmail.com>
2017-03-26 12:04:39 -0700

    color by ingress port

--- mapper.py
+++ mapper.py
@@ -1,4 +1,6 @@
 #!/user/bin/python env
+import json
+import random
 import sys
 
 import boto3
@@ -15,9 +17,27 @@ class SGraph(object):
         self.dot = Digraph()
         self.dot.format = 'svg'
         self.sgs = security_groups
+        self.colors = self.load_colors()
+        self.colors_assigned = []
         self.create_nodes()
         self.create_edges()
         self.dot.render('img/{}.svg'.format(outfile))
+
+    def load_colors(self, colors_file='colors.json'):
+        with open(colors_file) as f:
+            return json.load(f)
+
+    def select_color(self):
+            available_colors = list(
+                                set(self.colors) - set(self.colors_assigned)
+                               )
+            if len(available_colors) > 0:
+                color = random.choice(available_colors)
+                self.colors_assigned.append(color)
+                return color
+            else:
+                print('out of colors')
+                return 'black'
 
     def create_nodes(self):
         for group in self.sgs:
@@ -33,11 +53,15 @@ class SGraph(object):
         return ingress
 
     def create_edges(self):
+        port_to_color = {}
         for src_g in self.sgs:
             ingress = self.get_ingress(src_g)
             for dst_g, port in ingress.items():
+                if port not in port_to_color:
+                    port_to_color[port] = self.select_color()
                 self.dot.edge(
-                    src_g.group_name, dst_g, label=str(port)
+                    src_g.group_name, dst_g, label=str(port),
+                    color=port_to_color[port]
                 )
 
 region_name = sys.argv[1]
